156f256c2d3df6af04b23a61038c42a89bd4e325
demix007 <ayodejiabidemi007@gmail.com>
2023-05-18 22:01:52 +0100

    configure database

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root "messages#index"
+  get 'api/random_greeting', to: 'messages#index'
+end

f3be42f3c4f22d1bfca52f6dec2cf6709d0cbdc0
demix007 <ayodejiabidemi007@gmail.com>
2023-05-19 10:53:57 +0100

    Modify routes

--- app/controllers/messages_controller.rb
+++
@@ -1,6 +0,0 @@
-class MessagesController < ApplicationController
-  def index
-    @msg = Message.find(Message.pluck(:id).sample)
-    render json: @msg
-  end
-end
--- app/models/greeting.rb
+++
@@ -1,3 +0,0 @@
-class Greeting < ActiveRecord::Base
-  # class implementation goes here
-end
